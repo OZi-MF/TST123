dc907e3c565e6cbe71303ea35d0421fd571d26b4
unknown <unknown>
2023-03-28 13:20:13 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+load.initialize('Initialize', async function() {
+	
+});
+
+load.action('Action', async function() {
+	// put some actions here
+});
+
+load.finalize('Finalize', async function() {
+	
+});
